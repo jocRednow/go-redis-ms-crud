eda7579a775feb0568572ff8f91e9c19461557eb
stepankrasnov <s.krasnov@spacepass.pro>
2024-07-02 14:44:57 +0200

    Basic webserver added

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to lesten to server.", err)
+	}
+
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
 }
